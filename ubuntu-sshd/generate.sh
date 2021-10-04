1785854f8919dfa4ea408ee78adff20f57e799d7
Amatist_kurisu <misaki.zhcy@gmail.com>
2021-10-04 18:18:42 +0800

    dev

--- ubuntu-sshd/generate.sh
+++ ubuntu-sshd/generate.sh
@@ -4,4 +4,9 @@ set -e
 for v in */; do
   v="${v%/}"
   sed "s/%VERSION%/$v/g" Dockerfile.template > "$v/Dockerfile"
+  imageTag="kurisux/ubuntu-sshd-ex:${v}"
+  cd $v
+  docker build -t $imageTag .
+  docker push $imageTag
+  cd -
 done
