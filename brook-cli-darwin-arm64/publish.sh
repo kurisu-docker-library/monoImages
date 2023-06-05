271688148eda730f9a3874330c9867097f168c56
Amatist_kurisu <misaki.zhcy@gmail.com>
2023-06-05 14:44:37 +0800

    dev

--- brook-cli-darwin-arm64/publish.sh
+++ brook-cli-darwin-arm64/publish.sh
@@ -7,7 +7,7 @@ TAG="darwin_arm64"  # 你可以修改为你需要的标签
 docker rmi -f ${IMAGE_NAME}:${TAG}
 
 # 在当前目录构建Docker镜像
-docker build --no-cache -t ${IMAGE_NAME}:${TAG} .
+docker build --platform linux/arm64 --no-cache -t ${IMAGE_NAME}:${TAG} .
 
 # 将镜像推送到Docker Hub
 docker push ${IMAGE_NAME}:${TAG}
