e6ebf90efcff10aaf9e019d0ffd2184f5a4f64d6
Amatist_kurisu <misaki.zhcy@gmail.com>
2023-06-05 14:33:40 +0800

    dev

--- brook-cli-amd64/publish.sh
+++ brook-cli-amd64/publish.sh
@@ -6,7 +6,7 @@ TAG="latest"  # 你可以修改为你需要的标签
 docker rmi -f ${IMAGE_NAME}:${TAG}
 
 # 在当前目录构建Docker镜像
-docker build -t ${IMAGE_NAME}:${TAG} .
+docker build --no-cache -t ${IMAGE_NAME}:${TAG} .
 
 # 将镜像推送到Docker Hub
 docker push ${IMAGE_NAME}:${TAG}
