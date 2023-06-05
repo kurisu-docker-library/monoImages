0d5a552230257f2c4c318993d4d92a14f69bcb46
Amatist_kurisu <misaki.zhcy@gmail.com>
2023-06-05 15:01:51 +0800

    dev

--- brook-cli-linux-arm64/publish.sh
+++ brook-cli-linux-arm64/publish.sh
@@ -9,8 +9,8 @@ docker rmi -f ${IMAGE_NAME}:${TAG}
 # 创建 buildx 实例并使用
 docker buildx create --use
 
-# 在当前目录构建Docker镜像
-docker buildx build --platform linux/arm64 --no-cache -t ${IMAGE_NAME}:${TAG} .
+# 在当前目录构建Docker镜像，并推送到 Docker Hub
+docker buildx build --platform linux/arm64 --no-cache -t ${IMAGE_NAME}:${TAG} --push .
 
 # 将镜像推送到Docker Hub
 docker push ${IMAGE_NAME}:${TAG}
