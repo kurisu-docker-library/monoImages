c7320fc28b42dc48c8e3a0baf611a3b14bb0eb6c
Amatist_kurisu <misaki.zhcy@gmail.com>
2023-07-11 11:14:38 +0800

    dev

--- ubuntu-sshd/generate.sh
+++ ubuntu-sshd/generate.sh
@@ -10,3 +10,13 @@ for v in */; do
   docker push $imageTag
   cd -
 done
+
+for v in */; do
+  v="${v%/}"
+  sed "s/%VERSION%/$v/g" Dockerfile.template.armv8 > "$v/Dockerfile"
+  imageTag="kurisux/ubuntu-sshd-ex:armv8-${v}"
+  cd $v
+  docker build -t $imageTag .
+  docker push $imageTag
+  cd -
+done
